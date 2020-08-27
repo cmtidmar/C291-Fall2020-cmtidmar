77f618877a16ba343b181e985f71fbf17e99f267
Clare Tidmarsh <cmtidmar@iu.edu>
2020-08-27 12:57:33 -0400

    first commit of assignment0

--- exercises/wk1/hello.c
+++ exercises/wk1/hello.c
@@ -3,15 +3,19 @@
  * Author: Adeel Bhutta
 */
 
+
+// modify "hello.c" and change the file to print your name and how many days you have been alive. 
+//
+
 #include <stdio.h>
 
 int main(void) {
-  int myNumber = 0;
-  printf("Hello: Welcome to C291!\n");
-  printf("Please enter a number and press ENTER: ");
-  scanf("%d", &myNumber);  
-  printf("Here is the number yor entered: %d\n",myNumber);
-  return(0); 
-}
-
+ // int myNumber = 0;
+ // printf("Hello: Welcome to C291!\n");
+ // printf("Please enter a number and press ENTER: ");
+ // scanf("%d", &myNumber);  
+ // printf("Here is the number yor entered: %d\n",myNumber);
+ printf("Name: Clare Meichuan Tidmarsh\n");
+ printf("Days Alive: 7,590 days"); 
+ return(0); }
 
